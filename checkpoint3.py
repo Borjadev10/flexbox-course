52d4fee86dc7c0c5bdee073027b4f92bb6fb3640
borjadiv10 <150857335+Borjadev10@users.noreply.github.com>
2024-03-03 19:14:35 +0100

    Add files via upload

---
+++ checkpoint3.py
@@ -0,0 +1,51 @@
+#ejercicio1
+string = "ejercicio"
+numero = 5
+boolean = 7 > 10
+lista = ["Barcelona", "Madrid", "Sevilla"]
+print(string)
+print(numero)
+print(boolean)
+print(lista)
+
+#ejercicio2
+tres_primeras_letras = string[:3]
+print(tres_primeras_letras)
+
+
+#ejercicio3
+primer_elemento =lista[0]
+print(primer_elemento)
+
+
+#ejercicio4
+original= 9
+original_mas_diez = original +10
+print(original_mas_diez)
+
+#ejercicio5
+ultimo_elemento =lista[-1]
+print(ultimo_elemento)
+
+#ejercicio6
+names = 'harry,alex,susie,jared,gail,conner'
+lista_modificada = names.split(",")
+print(lista_modificada)
+
+#ejercicio7
+lista_modificada_dos = lista_modificada[0] .upper() 
+lista_modificada_tres =lista_modificada[1:]
+print(lista_modificada_dos)
+print(lista_modificada_dos, lista_modificada_tres)
+
+#ejercicio8
+ejercicio8 = f"antes del 6 va el {numero}"
+print(ejercicio8)
+
+#ejercicio9
+print("hello World")
+
+
+
+
+
